fdda167ae591d5275d9bb9392040860e8737b44d
Rehnuma Bano <70592871+Rehnuma111@users.noreply.github.com>
2023-03-11 00:27:30 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+
+const api_config = {
+  db_url: "mongodb+srv://Rehnuma11:1212@cluster0.mkx69tw.mongodb.net/3DModal?retryWrites=true&w=majority"
+};
+
+module.exports = api_config;
